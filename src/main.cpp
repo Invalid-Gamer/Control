b44aa475ffb04ecd0bbf5b2e0173810c10a51586
Invalid-Gamer <87494696+Invalid-Gamer@users.noreply.github.com>
2026-04-02 16:14:44 +0200

    Libraries, Pin Config/I2C Config

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
-#include <Arduino.h>
+#include <Arduino.h> //Arduino Pkg
+#include <Wire.h> // Display
+#include <LiquidCrystal_I2C.h> // Display
+#include <WiFi.h> // Internet
+#include <Preferences.h> // EPROM Mäßige Speicherung von Config (Nicht im Code)
 
-// put function declarations here:
-int myFunction(int, int);
+// Preferences: WiFim etc. - konstante Variablen - über Serial überschreibbar, nicht im Code vorhanden
+Preferences prefs;
+String WIFI_SSID, WIFI_PASS; // Captain WiFi
+String TARGET_IP, TARGET_PORT; // Captain Target
+String DEVICE_NAME; // WiFi Name
+
+// Joystick Config
+const int Btn_Pin = 17; // Button vom Joystick
+const int Vrx_Pin = 32; // X-Richtung
+const int Vry_Pin = 0; // Y-Richtung
+
+// LCD Config
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C Addresse, 16x2 Characters Init.
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }

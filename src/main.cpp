7914a451604c4065e883eebf96403b9c1e111a1c
Invalid-Gamer <87494696+Invalid-Gamer@users.noreply.github.com>
2026-04-02 23:29:51 +0200

    Mehr Setup, Mehr Doku, Mehr Config

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 #include <WiFi.h> // Internet
 #include <Preferences.h> // EPROM Mäßige Speicherung von Config (Nicht im Code)
 
-// Preferences: WiFim etc. - konstante Variablen - über Serial überschreibbar, nicht im Code vorhanden
+// Preferences: WiFi etc. - konstante Variablen - über Serial überschreibbar, nicht im Code vorhanden
 Preferences prefs;
-String WIFI_SSID, WIFI_PASS; // Captain WiFi
-String TARGET_IP, TARGET_PORT; // Captain Target
-String DEVICE_NAME; // WiFi Name
+String WiFi_SSID, WiFi_Pass; // Captain WiFi
+String Target_IP, Target_Port; // Captain Target
+String Device_Name; // WiFi Module Name
 
 // Joystick Config
 const int Btn_Pin = 17; // Button vom Joystick
@@ -19,13 +19,18 @@ const int Vry_Pin = 32; // Y-Richtung
 LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C Addresse, 16x2 Characters Init.
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200); // Serial Comms (Preferences ändern, debug)
+
+  // Pins Setup
   pinMode(Btn_Pin, INPUT);
   pinMode(Vrx_Pin, INPUT);
   pinMode(Vry_Pin, INPUT);
+
+  // LCD Setup
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }

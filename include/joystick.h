f1c7fc116f3fe6132ceb8c7e0c54fb822786613e
Invalid-Gamer <87494696+Invalid-Gamer@users.noreply.github.com>
2026-04-06 19:56:59 +0200

    Joystick funktionen

--- include/joystick.h
+++ include/joystick.h
@@ -1,6 +1,17 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
 
+#include <Arduino.h>
 
+const int limit_left = 1500;
+const int limit_right = 2600;
+
+struct JoystickRaw {
+    int x;
+    int y;
+    bool btn;
+};
+
+JoystickRaw getRawJoystick();
 
 #endif
--- src/joystick.cpp
+++ src/joystick.cpp
@@ -1,3 +1,22 @@
 #include <Arduino.h>
 #include <global.h>
 #include <joystick.h>
+
+JoystickRaw getRawJoystick() {
+    JoystickRaw data;
+    data.x = analogRead(Vrx_Pin);
+    data.y = analogRead(Vry_Pin);
+    data.btn = digitalRead(Btn_Pin);
+    return data;
+}
+
+void joyStickMenu() {
+    JoystickRaw currentPos = getRawJoystick();
+    if(currentPos.x > limit_right) {
+        if(advancedLog){Serial.println("Menu right");}
+    } else if(currentPos.y < limit_left) {
+        if(advancedLog){Serial.println("Menu left");}
+    } else if (currentPos.btn) {
+        if(advancedLog){Serial.println("Button pressed");}
+    }
+}
